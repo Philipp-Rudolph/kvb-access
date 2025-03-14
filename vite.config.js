230af7108f739ecc035b9a0dfff0179c89d79e14
Philipp-Rudolph <phil.rudolph@ymail.com>
2025-03-14 14:30:55 +0100

    fix: allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // To allow external connections
     port: 9090, // Default port for development
+    allowedHosts: ['www.kvb-access.de'],
     proxy: {
       '/api': {
         target: 'https://data.webservice-kvb.koeln/service/opendata', // Use for macOS/Windows, use host IP for Linux
